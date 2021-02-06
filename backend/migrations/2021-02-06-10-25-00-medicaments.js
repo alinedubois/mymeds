f6bcbdd8c6c7eb8b5dd3a807a81e16ff2dfd01f0
Vincent Dubois <v.dubois@lectra.com>
2021-02-06 11:29:58 +0100

    fix intégration du fichier des médicaments

--- backend/migrations/2021-02-06-10-25-00-medicaments.js
+++ backend/migrations/2021-02-06-10-25-00-medicaments.js
@@ -11,20 +11,7 @@ module.exports = {
 
         const contenuDuFichierDesMedicaments = fs.readFileSync(__dirname + '/medicaments.txt', 'latin1');
         await stockageEnLigne.writeFile('medicaments.txt', Buffer.from(contenuDuFichierDesMedicaments));
-/*
-        const medicamentsAuFormatTexte = fs.readFileSync(, 'latin1').split('\r\n');
 
-        const medicaments = medicamentsAuFormatTexte.map(medicament => {
-            const informationsMedicament = medicament.split("\t");
-            return {
-                identifiant : informationsMedicament[0],
-                nom : informationsMedicament[1],
-                formePharmaceutique : informationsMedicament[2],
-                voieAdministration : informationsMedicament[3],
-                surveillanceRenforcee : informationsMedicament[11]
-            };
-        }).filter(medicament => medicament.nom && medicament.nom.trim() !== '');
-*/
         return Promise.resolve();
     },
     down: async () => {
