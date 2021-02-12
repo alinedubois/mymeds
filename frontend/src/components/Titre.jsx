432539aafb2b1416b47081eeaeab906dac9a50c7
Aline DUBOIS <juillet.aline@gmail.com>
2021-02-12 16:33:59 +0100

    décomposition des composants de la barre de navigation

---
+++ frontend/src/components/Titre.jsx
@@ -0,0 +1,10 @@
+import Typography from "@material-ui/core/Typography";
+import React from "react";
+import './BarreDeNavigation.css';
+
+export  const Titre = () => {
+    return (<Typography variant="h6" noWrap className="AppTitre">
+        My Meds
+    </Typography>);
+
+};
