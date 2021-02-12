5ee01a057795385dc6d5cb857ad1529745a0aa1f
Aline DUBOIS <juillet.aline@gmail.com>
2021-02-12 16:33:59 +0100

    décomposition des composants de la barre de navigation

--- frontend/src/components/BarreDeNavigation.jsx
+++ frontend/src/components/BarreDeNavigation.jsx
@@ -1,89 +1,25 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import Typography from "@material-ui/core/Typography";
-import {Avatar, CircularProgress, Tooltip} from "@material-ui/core";
-import Menu from "@material-ui/core/Menu/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
 import AppBar from "@material-ui/core/AppBar";
 import {useStyles} from "../useStyles";
-import {useAuth0} from "@auth0/auth0-react";
 import './BarreDeNavigation.css';
+import {BoutonMenu} from "./BoutonMenu";
+import {Titre} from "./Titre";
+import {IdentificationAvatar} from "./IdentificationAvatar";
 
 export const BarreDeNavigation = (props) => {
-    const { user, isAuthenticated, isLoading, loginWithRedirect, logout } = useAuth0();
 
-    useEffect(() => {
-        if (!isAuthenticated && !isLoading) {
-            loginWithRedirect();
-        }
-    });
     const classes = useStyles();
-    const [anchorEl, setAnchorEl] = useState(null);
-    const open = Boolean(anchorEl);
-    const handleMenu = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+
     return (
         <AppBar position="fixed" className={classes.appBar}>
             <Toolbar>
-                <IconButton
-                    color="inherit"
-                    aria-label="open drawer"
-                    edge="start"
-                    onClick={props.basculerMenu}
-                    className={classes.menuButton}
-                >
-                    <MenuIcon />
-                </IconButton>
+                <BoutonMenu basculerMenu={props.basculerMenu}/>
                 <div className="AppBarContenu">
-                    <Typography variant="h6" noWrap className="AppTitre">
-                        My Meds
-                    </Typography>
-                    {isLoading && (
-                        <CircularProgress />
-                    )}
-                    {isAuthenticated && (
-                        <div className="AppBarIcones">
-                            <IconButton
-                                aria-label="account of current user"
-                                aria-controls="menu-appbar"
-                                aria-haspopup="true"
-                                onClick={handleMenu}
-                                color="inherit"
-                            >
-                                <Tooltip title={user.name}>
-                                    <Avatar
-                                        alt={user.name}
-                                        src={user.picture} />
-                                </Tooltip>
-                            </IconButton>
-                            <Menu
-                                id="menu-appbar"
-                                anchorEl={anchorEl}
-                                anchorOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                open={open}
-                                onClose={handleClose}
-                            >
-                                <MenuItem onClick={()=>{}}>Profile</MenuItem>
-                                <MenuItem onClick={() => logout()}>Déconnexion</MenuItem>
-                            </Menu>
-                        </div>
-                    )}
+                    <Titre/>
+                    <IdentificationAvatar/>
                 </div>
             </Toolbar>
         </AppBar>
     );
-}
+};
