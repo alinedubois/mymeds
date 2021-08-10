790b6e94c9bfcc96fa0e2b2abe2756530de8e771
Aline Dubois <juillet.aline@gmail.com>
2021-08-10 17:37:54 +0200

    ajout de l'authentification

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,18 +1,27 @@
-import {Component} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {Medicament} from "./fiche-medicament/fiche-medicament.component";
 import {ReferentielMedicamentsService} from "./referentiel-medicaments.service";
 import {AuthService} from "@auth0/auth0-angular";
+import {DOCUMENT} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   medicaments: Array<Medicament> = [];
   valeur="coucou";
 
-  constructor(public auth: AuthService, private referentiel: ReferentielMedicamentsService) {
+  constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService, private referentiel: ReferentielMedicamentsService) {
     this.referentiel.medicaments().subscribe(medicamentsResponse=>this.medicaments=medicamentsResponse);
+  }
+
+  ngOnInit(): void {
+    this.auth.isAuthenticated$.subscribe(authenticated => {
+      if (!authenticated) {
+        this.auth.loginWithRedirect();
+      }
+    });
   }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {AuthModule} from "@auth0/auth0-angular";
 import {MatIconModule} from "@angular/material/icon";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -25,7 +28,10 @@ import {MatIconModule} from "@angular/material/icon";
     HttpClientModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatMenuModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
