521f1ab78c46f9a70545ab21c0d248d79859805e
Aline Dubois <juillet.aline@gmail.com>
2021-08-17 21:35:06 +0200

    creation du composant et du service pharmacie front-end

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FicheMedicamentComponent } from './fiche-medicament/fiche-medicament.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FicheMedicamentComponent} from './fiche-medicament/fiche-medicament.component';
 import {HttpClientModule} from "@angular/common/http";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {AuthModule} from "@auth0/auth0-angular";
 import {MatIconModule} from "@angular/material/icon";
@@ -13,12 +13,13 @@ import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 import {MatMenuModule} from "@angular/material/menu";
 import {MatButtonModule} from "@angular/material/button";
 import {MatAutocompleteModule} from "@angular/material/autocomplete";
-import { AppBarComponent } from './app-bar/app-bar.component';
-import { CompteUtilisateurComponent } from './app-bar/compte-utilisateur/compte-utilisateur.component';
-import { AjoutMedicamentComponent } from './app-bar/ajout-medicament/ajout-medicament.component';
+import {AppBarComponent} from './app-bar/app-bar.component';
+import {CompteUtilisateurComponent} from './app-bar/compte-utilisateur/compte-utilisateur.component';
+import {AjoutMedicamentComponent} from './app-bar/ajout-medicament/ajout-medicament.component';
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {ReactiveFormsModule} from "@angular/forms";
 import {MatInputModule} from "@angular/material/input";
+import {PharmacieComponent} from './pharmacie/pharmacie.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import {MatInputModule} from "@angular/material/input";
     AppBarComponent,
     CompteUtilisateurComponent,
     AjoutMedicamentComponent,
+    PharmacieComponent,
   ],
   imports: [
     BrowserModule,
