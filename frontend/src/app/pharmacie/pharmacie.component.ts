447d2782a1aff1a9f83dec04a85dd834f851c94c
Aline Dubois <juillet.aline@gmail.com>
2022-03-12 18:52:33 +0100

    finalisation de l'authentification

--- frontend/src/app/pharmacie/pharmacie.component.ts
+++ frontend/src/app/pharmacie/pharmacie.component.ts
@@ -3,6 +3,8 @@ import {BoiteDeMedicament, PharmacieService} from "./pharmacie.service";
 import {MatSnackBar} from "@angular/material/snack-bar";
 import {MatDialog} from "@angular/material/dialog";
 import {AjoutBoiteDeMedicamentDialogComponent} from "./ajout-boite-de-medicament-dialog/ajout-boite-de-medicament-dialog.component";
+import {AuthService} from "@auth0/auth0-angular";
+import {switchMap} from "rxjs/operators";
 
 @Component({
   selector: 'pharmacie',
@@ -16,33 +18,36 @@ export class PharmacieComponent implements OnInit {
   constructor(
     private pharmacieService: PharmacieService,
     private _snackBar: MatSnackBar,
-    private dialog : MatDialog
+    private dialog: MatDialog,
+    public auth: AuthService
   ) {
 
   }
 
   ngOnInit(): void {
-    this.pharmacieService.pharmacie('juillet.aline@gmail.com')
-      .subscribe(pharmacie => this.boitesDeMedicaments = pharmacie.boitesDeMedicaments);
+    this.auth.user$.pipe(
+      switchMap(user =>
+        this.pharmacieService.pharmacie(user?.email!!))
+    ).subscribe(pharmacie => this.boitesDeMedicaments = pharmacie.boitesDeMedicaments);
   }
 
   supprimerUneBoiteDeMedicament(boiteDeMedicamentId: number) {
-    this.pharmacieService.supprimerUneBoiteDeMedicament(boiteDeMedicamentId, 'juillet.aline@gmail.com')
-      .subscribe(() => {
-        this._snackBar.open('Suppression effectuée avec succès !', undefined, {
-          duration: 5000,
-          verticalPosition: "bottom",
-          horizontalPosition: "end",
-          panelClass: ['snack-bar-success']
-        });
-        this.boitesDeMedicaments = this.boitesDeMedicaments.filter(boiteDeMedicament => boiteDeMedicament.id !== boiteDeMedicamentId);
+    this.auth.user$.pipe(
+      switchMap(user =>
+        this.pharmacieService.supprimerUneBoiteDeMedicament(boiteDeMedicamentId, user?.email!!))
+    ).subscribe(() => {
+      this._snackBar.open('Suppression effectuée avec succès !', undefined, {
+        duration: 5000,
+        verticalPosition: "bottom",
+        horizontalPosition: "end",
+        panelClass: ['snack-bar-success']
       });
+      this.boitesDeMedicaments = this.boitesDeMedicaments.filter(boiteDeMedicament => boiteDeMedicament.id !== boiteDeMedicamentId);
+    });
   }
 
   ajouterUneBoiteDeMedicament() {
-    let dialogRef = this.dialog.open(AjoutBoiteDeMedicamentDialogComponent, {
-
-    });
+    let dialogRef = this.dialog.open(AjoutBoiteDeMedicamentDialogComponent, {});
     dialogRef.afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);
     });
