a44401aa42fc06a1bc762cbda25613d93b5d6fa5
Aline Dubois <juillet.aline@gmail.com>
2024-05-27 21:22:22 +0200

    changement de l'adresse du backend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   auth0Client: 'XXO7CW1nSrf08sIBQSGW5CMJqlS40tuw',
-  apiPath: 'https://mymeds-backend.herokuapp.com'
+  apiPath: 'http://mymeds.freeboxos.fr:16385'
 };
