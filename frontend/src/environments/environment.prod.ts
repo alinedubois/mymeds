0f112ea9bc74343c51e18a4fcac5d07abb39fe2c
Aline Dubois <juillet.aline@gmail.com>
2024-06-09 09:20:39 +0200

    changement de l'adresse du backend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   auth0Client: 'XXO7CW1nSrf08sIBQSGW5CMJqlS40tuw',
-  apiPath: 'http://mymeds.freeboxos.fr:16385'
+  apiPath: 'https://mymeds.ddns.net'
 };
